d5211a6e4828a7b9ec7ba4a126ae20bedd5d5365
RaphMt117 <rafa.mtorres117@outlook.com>
2023-08-16 20:40:00 -0300

    github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site:'https://raphmt117.github.io',
+  base:'/buqueChocoRosas',
     integrations: [tailwind()],
     experimental: {
         viewTransitions: true,
