727aa03bb932fc2b59d0758f5766401154d20df2
RaphMt117 <rafa.mtorres117@outlook.com>
2023-08-16 21:33:15 -0300

    .

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,13 +2,6 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		// colors: {
-		// 	'cinza-escuro': '#33342f',
-		// 	'lavanda': '#d1b1c9',
-		// 	'roxo': '#6f205f',
-		// 	'marrom': '#a18a68',
-		// 	'marrom-claro': '#cab37d'
-		// },
 		extend: {},
 	},
 	plugins: [],
