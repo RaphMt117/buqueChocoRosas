f8173bc9429e0c4e24257fc06d56468c459ca6fc
RaphMt117 <rafa.mtorres117@outlook.com>
2023-08-19 23:12:59 -0300

    introduced prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import {defineConfig} from 'astro/config'
+import tailwind from '@astrojs/tailwind'
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  experimental: {
-    viewTransitions: true,
-    assets: true
-  },
-  output: "server",
-  adapter: vercel()
-});
+	integrations: [tailwind()],
+	experimental: {
+		viewTransitions: true,
+		assets: true,
+	},
+	output: 'server',
+	adapter: vercel(),
+})
--- src/links.ts
+++ src/links.ts
@@ -1,34 +1,33 @@
 export const mainContent = {
-  imgSrc: `/src/assets/images/col-logo-rosa-e-roxo.png`, // caminho para imagem
-  name: `Buquê Choco Rosas`, // alt
-  desc: `texto simples descritivo` // descrição
-};
-
+	imgSrc: `/src/assets/images/col-logo-rosa-e-roxo.png`, // caminho para imagem
+	name: `Buquê Choco Rosas`, // alt
+	desc: `texto simples descritivo`, // descrição
+}
 
 export const socialLinks = [
-    {
-        text: 'Whatsapp',
-        icon: 'whatsapp',
-        url: 'https://wa.me//553194948160?text=Oii,%20te%20achei%20pelo%20site!%20Gostaria%20de%20realizar%20uma%20encomenda.'
-    },
-    {
-        text: 'Instagram',
-        icon: 'instagram',
-        url: 'https://www.instagram.com/buquechocorosas/'
-    },
+	{
+		text: 'Whatsapp',
+		icon: 'whatsapp',
+		url: 'https://wa.me//553194948160?text=Oii,%20te%20achei%20pelo%20site!%20Gostaria%20de%20realizar%20uma%20encomenda.',
+	},
+	{
+		text: 'Instagram',
+		icon: 'instagram',
+		url: 'https://www.instagram.com/buquechocorosas/',
+	},
 ]
 
 export const links = [
-    {
-        text: 'Início',
-        url: '/'
-    },
-    {
-        text: 'Produtos',
-        url: '/produtos'
-    },
-    {
-        text: 'Sobre',
-        url: '/sobre'
-    },
+	{
+		text: 'Início',
+		url: '/',
+	},
+	{
+		text: 'Produtos',
+		url: '/produtos',
+	},
+	{
+		text: 'Sobre',
+		url: '/sobre',
+	},
 ]
