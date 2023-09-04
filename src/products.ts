a290cfca9ff22c58530734f349ec22112bab53e6
RaphMt <rafa.mtorres117@outlook.com>
2023-09-04 17:53:03 -0300

    buques page init

---
+++ src/products.ts
@@ -0,0 +1,74 @@
+export const buques = [
+    {
+        id: '101',
+        nome: 'Buque de Chocolate',
+        desc: 'Um delicioso buque de chocolate nhamnham',
+        preco: 64.90,
+        image: 'buque-chocolate.jpg',
+        ingredientes: ['Côco', 'Chocolate', 'Baunilha']
+    },
+    {
+        id: '102',
+        nome: 'Buque de teste teste',
+        desc: 'Um delicioso buque de chocolate nhamnham',
+        preco: 67.90,
+        image: 'buque-chocolate',
+        ingredientes: ['Côco', 'Chocolate', 'Baunilha']
+    },
+    {
+        id: '103',
+        nome: 'Buque de teste teste teste',
+        desc: 'Um delicioso buque de chocolate nhamnham',
+        preco: 65.90,
+        image: 'buque-chocolate',
+        ingredientes: ['Côco', 'Chocolate', 'Baunilha']
+    },
+    {
+        id: '104',
+        nome: 'Buque quatro',
+        desc: 'Um delicioso buque de chocolate nhamnham',
+        preco: 62.90,
+        image: 'buque-chocolate',
+        ingredientes: ['Côco', 'Chocolate', 'Baunilha']
+    },
+    {
+        id: '105',
+        nome: 'Buque de Chocolate',
+        desc: 'Um delicioso buque de chocolate nhamnham',
+        preco: 61.90,
+        image: 'buque-chocolate',
+        ingredientes: ['Côco', 'Chocolate', 'Baunilha']
+    },
+    {
+        id: '106',
+        nome: 'Buque de Chocolate',
+        desc: 'Um delicioso buque de chocolate nhamnham',
+        preco: 64.90,
+        image: 'buque-chocolate',
+        ingredientes: ['Côco', 'Chocolate', 'Baunilha']
+    },
+    {
+        id: '107',
+        nome: 'Buque de Chocolate',
+        desc: 'Um delicioso buque de chocolate nhamnham',
+        preco: 64.90,
+        image: 'buque-chocolate',
+        ingredientes: ['Côco', 'Chocolate', 'Baunilha']
+    },
+    {
+        id: '108',
+        nome: 'Buque de Chocolate',
+        desc: 'Um delicioso buque de chocolate nhamnham',
+        preco: 64.90,
+        image: 'buque-chocolate',
+        ingredientes: ['Côco', 'Chocolate', 'Baunilha']
+    },
+    {
+        id: '109',
+        nome: 'Buque de Chocolate',
+        desc: 'Um delicioso buque de chocolate nhamnham',
+        preco: 64.90,
+        image: 'buque-chocolate',
+        ingredientes: ['Côco', 'Chocolate', 'Baunilha']
+    },
+]
